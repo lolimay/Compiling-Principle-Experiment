f5d1264c4dc4c527866cffb7ecf0a8f80b2e6da5
lolimay <lolimay@lolimay.cn>
2019-12-04 01:28:40 +0800

    Remove unnecessary deps

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,8 @@
 import { isLowerCase, isUpperCase } from './char';
-import { print } from './log';
 import { concatMessage } from './message';
 
 export {
     concatMessage,
     isLowerCase,
-    isUpperCase,
-    print
+    isUpperCase
 };
